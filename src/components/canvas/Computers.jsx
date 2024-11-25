5ffc3aaa28678f4739992b10ff7326db8a341893
Moheb Yousry EXT <moheby183@gmail.com>
2024-11-25 11:08:03 +0200

    last fix

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -61,7 +61,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
