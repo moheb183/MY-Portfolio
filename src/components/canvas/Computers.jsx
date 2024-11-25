0d83f90c9f907cfb8a48c68096e5974948f1b1f1
Moheb Yousry EXT <moheby183@gmail.com>
2024-11-25 10:54:05 +0200

    mf6

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,27 +1,37 @@
 import { useEffect, useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import Loader from "../Loader";
 
+// تفعيل DRACO
+const useDRACOLoader = (url) => {
+  const loader = new GLTFLoader();
+  const dracoLoader = new DRACOLoader();
+  dracoLoader.setDecoderPath("/draco/"); // حدد مسار DRACO الخاص بك
+  loader.setDRACOLoader(dracoLoader);
+  return loader.load(url);
+};
+
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  // تحميل النموذج باستخدام DRACO
+  const computer = useGLTF("./desktop_pc/scene.gltf", useDRACOLoader);
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.05} groundColor="black" />
       <directionalLight
         position={[0, 5, 5]}
-        intensity={2}
-        castShadow
-        shadow-mapSize={1024}
+        intensity={1}
+        castShadow={false} // إيقاف الظلال لتقليل الاستهلاك
       />
-      <pointLight intensity={1} />
+      <pointLight intensity={0.5} /> {/* تقليل شدة الضوء */}
       <primitive
-        receiveShadow
+        receiveShadow={false} // إيقاف الظلال إذا كانت غير ضرورية
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -2, -1.5]}
+        scale={isMobile ? 0.5 : 0.7} // تقليل الحجم بشكل أكبر للأجهزة المحمولة
+        position={isMobile ? [0, -2, -2] : [0, -2, -1.5]} // تعديل الموضع
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -46,17 +56,18 @@ const ComputersCanvas = () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
+
   return (
     <Canvas
       frameloop="demand"
       shadows
-      dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: false }}
+      dpr={[1, 1.5]} // تقليل دقة العرض لتقليل استهلاك الذاكرة
+      camera={{ position: [20, 3, 5], fov: 30 }} // تقليل مجال الرؤية
+      gl={{ preserveDrawingBuffer: false }} // تعطيل الاحتفاظ بالصورة لتقليل الذاكرة المستهلكة
     >
       <Suspense fallback={<Loader />}>
         <OrbitControls
-          autoRotate
+          autoRotate={false} // إيقاف التدوير التلقائي للكاميرا لتقليل استهلاك الموارد
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
@@ -68,4 +79,5 @@ const ComputersCanvas = () => {
     </Canvas>
   );
 };
+
 export default ComputersCanvas;
